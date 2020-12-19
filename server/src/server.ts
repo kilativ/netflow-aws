15b79e923ba42aed393467ca0e3378461e9312a6
kilativ@gmail.com <kilativ@gmail.com>
2020-12-18 19:41:40 -0500

    Deleted passport

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
-import passport from 'passport';
 import session from 'express-session';
-import { AuthRoutes } from './server-auth'
 import { PlaidRoutes } from './server-plaid';
 import { AccountDal } from './dal/account';
-import  {GoogleAuth, OAuth2Client } from 'google-auth-library';
 import { google } from 'googleapis';
 
 
@@ -22,11 +19,8 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(session({ secret: '4101b528-66a7-4e9a-8241-48a7c9ff5bd7' }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(bodyParser.json())
 
-AuthRoutes.Add(app);
 PlaidRoutes.Add(app);
 
 app.get('/', (req, res) => {
@@ -40,26 +34,26 @@ const validateUser = function (req: any, res: any, next: any) {
 
   let token = req.headers["authorization"] as string;
   token = token.substr(7);
-  
+
   var OAuth2 = google.auth.OAuth2;
   var oauth2Client = new OAuth2();
-  oauth2Client.setCredentials({access_token: token});
-var oauth2 = google.oauth2({
-  auth: oauth2Client,
-  version: 'v2'
-});
-oauth2.userinfo.get(
-  function(err, res) {
-    if (err) {
-       console.log(err);
-    } else {
-       console.log(res);
-    }
-});
+  oauth2Client.setCredentials({ access_token: token });
+  var oauth2 = google.oauth2({
+    auth: oauth2Client,
+    version: 'v2'
+  });
+  oauth2.userinfo.get(
+    function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res);
+      }
+    });
 
 
 
-  
+
   next()
 };
 
