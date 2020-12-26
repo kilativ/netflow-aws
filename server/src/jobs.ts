23d0144be42e77fc20f72c0882855607c9ef7910
kilativ@gmail.com <kilativ@gmail.com>
2020-12-26 15:39:43 -0500

    More Logging

--- server/src/jobs.ts
+++ server/src/jobs.ts
@@ -8,23 +8,31 @@ import { Formatter } from '../../shared/utils/formatter';
 
 export class Transactions {
     async saveToDb() {
+        console.log('saveToDb job');
         const accountDal = new AccountDal();
         const netFlowUsers = await accountDal.getAll();
+        console.log(`got ${netFlowUsers.length} users`);
 
         const transactionDal = new TransactionDal()
         const plaidDal = new PlaidDal();
         const balanceDal = new BalanceDal();
         const today = Formatter.toISODateString(new Date());
+        console.log(`retrieval date ${today}`);
 
         for (const user of netFlowUsers) {
             for (const bank of user.banks) {
+                console.log(`processing user ${user.userId}, bank ${bank.id}`);
+
                 const txnsAndAccounts = await plaidDal.fetchTransactions(bank.token);
+                console.log(`got ${txnsAndAccounts.accounts.length} accounts and ${txnsAndAccounts.transactions.length} transactions from plaid`)
 
                 // don't run it for a lot of transaction until batching is implemented in order not to exceed DynamoDB througput limits
                 txnsAndAccounts.transactions.forEach(txn => transactionDal.update(txn));
+                console.log('updated transactions in dynamodb');
 
                 const accounts = txnsAndAccounts.accounts;
                 accountDal.updateBankAccounts(user.userId, bank.id, accounts);
+                console.log('updated banks in user object in dynamodb');
 
                 accounts.forEach(acct => {
                     const balance = acct.balances as BalanceDto;
@@ -33,6 +41,7 @@ export class Transactions {
 
                     balanceDal.update(balance);
                 })
+                console.log('updated balances object in dynamodb');
             }
             
         }
