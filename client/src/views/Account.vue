40eff0956c7dbe04ad4c88c03de9d12af269ff06
kilativ@gmail.com <kilativ@gmail.com>
2021-01-10 14:20:41 -0500

    Started on tailwind css template

--- client/src/views/Account.vue
+++ client/src/views/Account.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="account">
-    <h1>This is my account page</h1>
-    <h1>IsInit: {{ Vue3GoogleOauth.isInit }}</h1>
-    <h2>IsAuthorized: {{ Vue3GoogleOauth.isAuthorized }}</h2>
-    <h3 v-if="user">signed user: {{user.userId}}</h3>
+  <div>
 
-    <h2>Accounts</h2>
-    <div v-for="bank in list" :key="bank.id">
-      <h4>{{ bank.nickname }}</h4>
+  <div class="bg-gray-800 pt-3">
+      <div
+        class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white"
+      >
+        <h3 class="font-bold pl-2">Accounts</h3>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap">
+            
+      <div class="w-full md:w-1/1 xl:w-1/2 p-6"  v-for="bank in list" :key="bank.id">
+        <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+            <div class="flex-1 text-right md:text-center">
+              <h5 class="font-bold uppercase text-gray-600">Total Revenue</h5>
+              <h3 class="font-bold text-3xl">
+                {{ bank.nickname }}
+              </h3>
       <div v-for="account in bank.accounts" :key="account.account_id">
         <a v-bind:href="'/transactions/' + account.account_id">{{
           account.official_name ?? account.name
@@ -16,7 +26,15 @@
         {{ formatter.currencyUSD(account.balances.current) }}
         <a v-bind:href="'/snapshot/' + account.account_id">Snapshot</a>
       </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
+    <h1>IsInit: {{ Vue3GoogleOauth.isInit }}</h1>
+    <h2>IsAuthorized: {{ Vue3GoogleOauth.isAuthorized }}</h2>
+    <h3 v-if="user">signed user: {{user.userId}}</h3>
   </div>
 </template>
 <script lang="ts">
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="bg-gray-800 pt-3">
+      <div
+        class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white"
+      >
+        <h3 class="font-bold pl-2">Home sample</h3>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap">
+
+
+      
+      <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+        <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
+          <div class="flex flex-row items-center">
+            <div class="flex-shrink pr-4">
+              <div class="rounded-full p-5 bg-green-600">
+                <i class="fa fa-wallet fa-2x fa-inverse"></i>
+              </div>
+            </div>
+            <div class="flex-1 text-right md:text-center">
+              <h5 class="font-bold uppercase text-gray-600">Total Revenue</h5>
+              <h3 class="font-bold text-3xl">
+                $3249
+                <span class="text-green-500"
+                  ><i class="fas fa-caret-up"></i
+                ></span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+        <div class="bg-gradient-to-b from-pink-200 to-pink-100 border-b-4 border-pink-500 rounded-lg shadow-xl p-5">
+          <div class="flex flex-row items-center">
+            <div class="flex-shrink pr-4">
+              <div class="rounded-full p-5 bg-pink-600">
+                <i class="fas fa-users fa-2x fa-inverse"></i>
+              </div>
+            </div>
+            <div class="flex-1 text-right md:text-center">
+              <h5 class="font-bold uppercase text-gray-600">Total Users</h5>
+              <h3 class="font-bold text-3xl">
+                249
+                <span class="text-pink-500" >
+                  <i class="fas fa-exchange-alt"></i>
+                </span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import PlaidLink from '@/views/PlaidLink.vue'
+import PlaidLink from "@/views/PlaidLink.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    PlaidLink
-  }
-}
+    PlaidLink,
+  },
+};
 </script>
  
