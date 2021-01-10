05ad9c270716f1968b988b9a56b17ae44d8d08d6
kilativ@gmail.com <kilativ@gmail.com>
2021-01-10 15:08:05 -0500

    Balances as a grid

--- client/src/views/Account.vue
+++ client/src/views/Account.vue
@@ -14,19 +14,35 @@
       <div class="w-full md:w-1/1 xl:w-1/2 p-6"  v-for="bank in list" :key="bank.id">
         <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5">
             <div class="flex-1 text-right md:text-center">
-              <h5 class="font-bold uppercase text-gray-600">Total Revenue</h5>
-              <h3 class="font-bold text-3xl">
+              <h4 class="font-bold text-3xl">
                 {{ bank.nickname }}
-              </h3>
-      <div v-for="account in bank.accounts" :key="account.account_id">
-        <a v-bind:href="'/transactions/' + account.account_id">{{
-          account.official_name ?? account.name
-        }}</a>
-        - {{ account.mask }} ({{ account.type }} - {{ account.subtype }})
-        {{ formatter.currencyUSD(account.balances.current) }}
-        <a v-bind:href="'/snapshot/' + account.account_id">Snapshot</a>
-      </div>
+              </h4>
           </div>
+           <div class="p-5">
+                      <table class="w-full p-5 text-gray-700">
+                          <thead>
+                              <tr>
+                                  <th class="text-left text-blue-900">Name</th>
+                                  <th class="text-left text-blue-900">Number</th>
+                                  <th class="text-left text-blue-900">Type</th>
+                                  <th class="text-left text-blue-900">Balance</th>
+                                  <th class="text-left text-blue-900">Snapshot</th>
+                              </tr>
+                          </thead>
+
+                          <tbody>
+                              <tr v-for="account in bank.accounts" :key="account.account_id">
+                                  <td>
+                                    <a class="underline" v-bind:href="'/transactions/' + account.account_id">{{account.official_name ?? account.name}}</a>
+                                  </td>
+                                  <td>{{ account.mask }}</td>
+                                  <td>{{ account.type }} - {{ account.subtype }}</td>
+                                  <td>{{ formatter.currencyUSD(account.balances.current) }}</td>
+                                  <td><a class="underline" v-bind:href="'/snapshot/' + account.account_id">view</a></td>
+                              </tr>
+                          </tbody>
+                      </table>
+              </div>
         </div>
       </div>
 
